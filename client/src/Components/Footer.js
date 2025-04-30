40d875f008e5f7f0e4b0570ffef0cd67c1988e7e
nmamdali <nmamdali@gmail.com>
2025-04-30 13:34:22 +0400

    second commit

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>--Write your name--|©2024|PostIT|All Rights Reserved.</div>
+      <div>--Mohamed ali</div>
     </footer>
   );
 };
